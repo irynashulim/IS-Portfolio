faa93cb6448f83716928dcbead563956b35abc37
Iryna <iryna.shulim@gmail.com>
2021-09-25 19:47:31 -0500

    footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import { FaGithub, FaGoogle } from "react-icons/fa";
+function Footer({ links }) {
+  return (
+    <div className="footer">
+      <li
+        onClick={(e) => {
+          e.preventDefault();
+          window.location = `mailto: ${links.email}`;
+        }}
+      >
+        <FaGoogle />
+      </li>
+      <li onClick={() => window.open(links.github_page, "blank")}>
+        <FaGithub />
+      </li>
+    </div>
+  );
+}
+
+export default Footer;
